1ec3198ed97b2b6cc28e93c81cdefeffa02d2c3b
Mohammad Zanjani <mzanjani@au1.ibm.com>
2016-11-22 00:00:09 -0600

    Adding test

--- public/myd3.js
+++ public/myd3.js
@@ -1,9 +1,12 @@
-$.getJSON("https://3fdad485-b06b-4050-b67b-ad864dfeb7ee-bluemix.cloudant.com/ahudata/_all_docs", function(jsondata) {
-	
+document.write("test");
 
+$.getJSON("https://3fdad485-b06b-4050-b67b-ad864dfeb7ee-bluemix.cloudant.com/ahudata/_all_docs", function(jsondata) {
+	document.write("test1");
+	document.write("data");
+});
+document.write("test2");
 d3.select("body")
   .selectAll("p")
-  .data([jsondata, jsondata])
+  .data([1,2,3])
   .enter().append("p")
     .text(function(d) { return "I’m number " + d + "!"; });
-});
