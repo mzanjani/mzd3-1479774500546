139686a8c710400ffd99b4e295abb9ca47bd78f4
Mohammad Zanjani <mzanjani@au1.ibm.com>
2016-11-21 22:54:14 -0600

    P to li

--- public/myd3.js
+++ public/myd3.js
@@ -2,7 +2,7 @@ $.getJSON("https://3fdad485-b06b-4050-b67b-ad864dfeb7ee-bluemix.cloudant.com/ahu
 	
 
 d3.select("body")
-  .selectAll("p")
+  .selectAll("li")
   .data(jsondata)
   .enter().append("p")
     .text(function(d) { return "I’m number " + d + "!"; });
